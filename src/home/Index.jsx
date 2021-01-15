173fb80ae34ee3cbd10727027cc3430c13d98aa2
christian bell <christiantcbell@gmail.com>
2021-01-15 10:49:22 +0000

    add routine links

--- src/home/Index.jsx
+++ src/home/Index.jsx
@@ -46,13 +46,22 @@ function Home(amazonUrls) {
                 <h1>Hi Christian!</h1>
                 <p>You're logged in with React & JWT!!</p>
                 <br />
+                <p>Step one - link omni account to alexa account:</p>
                 <a href={amazonUrls.amazonUrls.alexa_app_url}> Alexa Installed - add omni to alexa!</a>
                 <br />
                 <br />
                 <a href={amazonUrls.amazonUrls.lwa_fallback_url}>Alexa not installed - Add Omni to Alexa!</a>
                 <br />
                 <br />
+                <p>Step two - add routine to alexa app:</p>
                 <br />
+                <a href={amazonUrls.amazonUrls.morning_routine}>Add morning routine (endpointId = 1)</a>
+                <br />
+                <br />
+                <a href={amazonUrls.amazonUrls.evening_routine}>Add evening routine (endpointId = 2)</a>
+                <br />
+                <br />
+                <p>Step three - trigger routine via smart home sensors (This is done by our BE eventually).</p>
                 <form onSubmit={handleSubmit}>
                   <input type="text" name="token" placeholder="Token"/>
                   <input type="text" name="endpointId" placeholder="endpointId"/>
